e61fbac44ae26330bdb3c442e6866c4d9307aacf
Omar Sotillo Franco <omarsotillofranco@gmail.com>
2021-06-20 21:07:52 +0200

    feat: ignores jsx files for ts-server

--- nvim/lua/lsp/js-ts-ls.lua
+++ nvim/lua/lsp/js-ts-ls.lua
@@ -10,7 +10,7 @@
 
 require'lspconfig'.tsserver.setup {
     cmd = {DATA_PATH .. "/lspinstall/typescript/node_modules/.bin/typescript-language-server", "--stdio"},
-    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+    filetypes = { "javascript", "typescript", "typescriptreact", "typescript.tsx" },
     on_attach = require'lsp'.tsserver_on_attach,
     -- This makes sure tsserver is not used for formatting (I prefer prettier)
     -- on_attach = require'lsp'.common_on_attach,
